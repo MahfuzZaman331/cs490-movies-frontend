6b4f3da9ffb4a710636420e71437b39c38aaec39
Mahfuz Zaman <zmahfuz331@gmail.com>
2025-09-23 15:26:08 -0400

    Add FilmDetails component

---
+++ src/FilmDetails.js
@@ -0,0 +1,14 @@
+// src/FilmDetails.js
+
+import React from 'react';
+
+function FilmDetails() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h2>Film Details</h2>
+      <p>This is where detailed film info will be displayed.</p>
+    </div>
+  );
+}
+
+export default FilmDetails;
